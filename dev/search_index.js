bcaf6a329fda4e92e06b1f6f751c0b670d28e716
Documenter.jl <documenter@juliadocs.github.io>
2026-07-17 16:21:57 +0000

    build based on 5d48387

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"category":"section","location":"#FixedTargetBackgrounds","page":"Home","text":"Documentation for FixedTargetBackgrounds.\n\n","title":"FixedTargetBackgrounds"}]
+}
